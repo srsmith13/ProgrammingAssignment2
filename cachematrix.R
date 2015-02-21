7c8ec356737ec8686348397d90078cb42d59130d
Savannah <savannah.reid.smith@gmail.com>
2015-02-21 09:59:34 -0500

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Takes a matrix as input, calculates the inverse and caches its value.
+## If inverse has already been calculate, retrieves value from cache.
+## If inverse has not already been calculated, computes, returns, and caches inverse
 
-## Write a short comment describing this function
+## MakeCacheMatrix creates a special matrix of functions that
+## Set the value of matrix
+## Get the galue of matrix
+## Set the inverse of the matrix
+## Get the inverse of the matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL ## set value of m to NULL as default
+    y <- NULL ## set value of y to NULL as default
+    set <- function(y) { ##set value of matrix
+        x <<- y ## caches input matrix
+        m <<- NULL ## set value of m to NULL if cacheSolve used
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve ##compute inverse of matrix and cache value
+    getinv <- function() m ##return matrix m
+    list <- list(set = set, get = get, setinv = setinv, getinv = getinv)
+    
 }
 
-
-## Write a short comment describing this function
+## cacheSolve calls the functions stored in the matrix created by makeCacheMatrix
+## If inverse already calculated, retrieve inverse from cache
+## If inverse not already calculated, compute, cache, and return inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv() ##apply getinv to matrix m, i.e. if inverse already calculated
+    if(!is.null(m)) { ##if m is not NULL i.e. if cacheSolve has been run before
+        message("getting cached matrix") ##alert that inverse already computed, retreiving from cache
+        return(m) ##return the inverse matrix
+    }
+    data <- x$get() ##if m is NULL, get input matrix
+    m <- solve(data, ...) ##compute inverse
+    x$setinv(m) ##run setinv function to cache inverse
+    m ##return inverse of matrix
 }
